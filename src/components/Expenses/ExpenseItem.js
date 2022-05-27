e012b9fdc062ce5013f3235744e2e150166d2ab7
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-05-27 21:51:28 +0300

    NewExpense.js,ExpenseForm.js and css files added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
-    const clickHandler = () => {
-        console.log('Clicked!!!');
-    }
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} amount={props.amount} title={props.title}/>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
-    );
-}
+  const [title, setTitle] = useState(props.title);  //React checks internally if state updated for the first time
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate
+        date={props.date}
+        amount={props.amount}
+        title={props.title}
+      />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
