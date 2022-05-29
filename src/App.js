97a4049d6d436c0752ad76f3d7a90dde7cb2ed3d
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-05-29 16:55:19 +0300

    Added Filter by years

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ const App = () => {
   ];
   const addExpenseHandler = (expense) => {
     console.log("In App.js");
-    console.log(expenses);
+    console.log(expense);
   };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
