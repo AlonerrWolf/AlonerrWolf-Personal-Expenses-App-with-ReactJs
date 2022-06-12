e2c5ffad2dc71cd4e56db2d3ab86bd9d558f0674
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-06-12 14:56:13 +0300

    no expenses found info added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,9 +9,20 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  const filteredExpense = props.items.filter(expense =>{
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -19,14 +30,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {filteredExpense.map((expense) => (
-          <ExpenseItem
-              key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
