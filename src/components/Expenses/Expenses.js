a973e308fff58f2708d758c6df64e248544e9c70
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-06-19 16:14:33 +0300

    added visual chart component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card.js";
 import ExpensesFilter from "./ExpensesFilter.js";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
@@ -21,6 +21,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items = {filteredExpenses}/>
       </Card>
     </div>
